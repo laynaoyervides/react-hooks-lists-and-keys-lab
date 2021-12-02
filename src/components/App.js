dbd340f463ac3525a8a3dddc0e62414e3ac7c7eb
Layna Oyervides <laynaoyervides@gmail.com>
2021-12-02 16:22:47 -0700

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
       <NavBar />
       <Home username={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <ProjectList key={user.projects.id} projects={user.projects} />
     </div>
   );
 }
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,20 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+ const newArray = projects.map(projects =>
+  {
+    return (
+      <ProjectItem key={projects.id} name={projects.name} about={projects.about} technologies={projects.technologies} />
+ );
+  })
+  console.log(newArray);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+          {/* render ProjectItem components here */}
+      {newArray};
+          </div>
     </div>
   );
 }
